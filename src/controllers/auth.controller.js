5a51ab55d2dc5a0ecc175081bf2a817223c39704
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-18 15:43:16 +0530

    login register is completed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -15,8 +15,8 @@ async function userRegisterController(req, res) {
             })
     }
     const user = await userModel.create({ name, email, password })
-// token ko hame cookies me set karna hoga for that we need to install cookie-parser package
-// npm install cookie-parser
+    // token ko hame cookies me set karna hoga for that we need to install cookie-parser package
+    // npm install cookie-parser
 
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
@@ -39,8 +39,40 @@ async function userRegisterController(req, res) {
 }
 //login controller
 async function userLoginController(req, res) {
-    const { email, password } = req.body;
-    
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email: email })
+    if (!user) {
+        return res.status(401).json(
+            {
+                message: "Email or Password is invalid",
+                status: "Failed"
+            }
+        )
+    }
+    const isValidPassword = await userModel.comparePassword(password, user.password)
+    if (!isValidPassword) {
+        return res.status(401).json(
+            {
+                message: "Email or Password is invalid",
+                status: "Failed"
+            }
+        )
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+    res.cookie("token", token)
+    return res.status(200).json(
+        {
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt
+            }, token
+        }
+    )
+
 
 }
 
